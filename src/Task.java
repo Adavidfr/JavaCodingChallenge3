25950c387c20f6d3fafeb3f8bc37cdb3f0c851f7
Alex David Frías <adavidfruiz@gmail.com>
2024-06-18 02:21:14 -0500

    feat: add task class

---
+++ src/Task.java
@@ -0,0 +1,68 @@
+import java.time.LocalDate;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+
+    public Task(int id, String title, String description, LocalDate dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueSate) {
+        this.dueDate = dueSate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
